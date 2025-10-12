4d08236034e49b15ca15eef00d3e6b6ea3cec48c
Pluto2051 <iluhabrovko@gmail.com>
2025-10-12 22:17:33 +0300

    вынес методы из main, стало визуально "Чище"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,107 +6,143 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int counter = 0; //Счётчик для проставления айди задачам
+    private static int counter = 0; //Счётчик для проставления айди задачам
+    private static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) throws IOException {
         while(true) { //Цикл программы
             System.out.println("Добавьте задачу. Доступные команды: add, list, remove, id, description, deadline, status, exit");
             String command = sc.nextLine();
             switch (command) {
                 case "add":
-                    System.out.println("Введите имя задачи:");
-                    Task task = new Task(sc.nextLine());
-                    task.setId(counter);
-                    counter ++;
-                    TaskManager.addTask(task);
+                    addTask();
                     break;
                 case "remove":
-                    System.out.println("Введите id задачи, которую нужно удалить:");
-                    TaskManager.listAllId();
-                    //цикл обработки исключения
-                    int idRem;
-                    while(true) {
-                        try {
-                            idRem = Integer.parseInt(sc.nextLine());
-                            if (TaskManager.findById(idRem) == "Id not found") {
-                                System.out.println("Введите корректный id");
-                            } else {
-                                break;
-                            }
-                        } catch (NumberFormatException exc) {
-                            System.out.println("Id должен быть в целочисленном формате!");
-                        }
-                    }
-                    TaskManager.removeTask(idRem);
+                    removeTask();
                     break;
                 case "list":
                     TaskManager.listAll();
                     break;
                 case "id":
-                    System.out.println("Введите айди задачи:");
-                    TaskManager.listAllId();
-                    System.out.println(TaskManager.findById(Integer.parseInt(sc.nextLine())));
+                    getById();
                     break;
                 case "description":
-                    System.out.println("Введите id задачи:");
-                    TaskManager.listAllId();
-                    int idDes;
-                    //цикл обработки исключения
-                    while(true) {
-                        try {
-                            idDes = Integer.parseInt(sc.nextLine());
-                            if (TaskManager.findById(idDes) == "Id not found") {
-                                System.out.println("Введите корректный id");
-                            } else {
-                                break;
-                            }
-                        } catch (NumberFormatException exc) {
-                            System.out.println("Id должен быть в целочисленном формате!");
-                        }
-                    }
-                    System.out.println("Введите описание для задачи: " + TaskManager.findById(idDes));
-                    String description = sc.nextLine();
-                    TaskManager.setDescription(idDes, description);
-                    System.out.println("Описание для задачи добавлено!");
+                    setDescription();
                     break;
                 case "status":
-                    System.out.println("Введите id задачи:");
-                    TaskManager.listAllId();
-                    int idSta;
-                    //цикл обработки исключения
-                    while(true) {
-                        try {
-                            idSta = Integer.parseInt(sc.nextLine());
-                            if (TaskManager.findById(idSta) == "Id not found") {
-                                System.out.println("Введите корректный id");
-                            } else {
-                                break;
-                            }
-                        } catch (NumberFormatException exc) {
-                            System.out.println("Id должен быть в целочисленном формате!");
-                        }
-                    }
-                    System.out.println("Введите статус для задачи (В работе, Готово, Отложено): " + TaskManager.findById(idSta));
-                    String status = sc.nextLine();
-                    TaskManager.setStatus(idSta, status);
-
-                    System.out.println("Статус для задачи добавлен!");
+                    setStatus();
                     break;
                 case "deadline":
-                    System.out.println("Введите id задачи:");
-                    TaskManager.listAllId();
-                    int idDead = Integer.parseInt(sc.nextLine());
-                    System.out.println("Введите конечную дату для задачи: " + TaskManager.findById(idDead));
-                    String deadline = sc.nextLine();
-                    TaskManager.setDeadline(idDead, deadline);
-                    System.out.println("Конечная дата для задачи установлена!");
+                    setDeadline();
                     break;
                 case "exit"://Выход из программы, цикла
                     System.out.println("Закрытие...");
                     return;
                 default://Случай несовпадения команды
                     System.out.println("Неизвестная команда");
+            }
+        }
+    }
+
+    private static void addTask() {
+        System.out.println("Введите имя задачи:");
+        Task task = new Task(sc.nextLine());
+        task.setId(counter);
+        counter ++;
+        TaskManager.addTask(task);
+    }
+
+    private static void removeTask() {
+        System.out.println("Введите id задачи, которую нужно удалить:");
+        TaskManager.listAllId();
+        //цикл обработки исключения
+        int idRem;
+        while(true) {
+                idRem = safeReadInt();
+                if (TaskManager.findById(idRem).equals("Id not found")) {
+                    System.out.println("Введите корректный id");
+                } else {
+                    break;
+                }
+        }
+        TaskManager.removeTask(idRem);
+    }
+
+    private static void setStatus() {
+        System.out.println("Введите id задачи:");
+        TaskManager.listAllId();
+        int idSta;
+        while (true) {
+            idSta = safeReadInt();
+            if (TaskManager.findById(idSta).equals("Id not found")) {
+                System.out.println("Введите корректный id");
+            } else {
+                break;
+            }
+        }
+        System.out.println("Введите статус для задачи (В работе, Готово, Отложено): " + TaskManager.findById(idSta));
+        String status = sc.nextLine();
+        TaskManager.setStatus(idSta, status);
+        System.out.println("Статус для задачи добавлен!");
+    }
+
+    private static void getById() {
+        System.out.println("Введите айди задачи:");
+        TaskManager.listAllId();
+        int id;
+        while(true) {
+            id = safeReadInt();
+            if(TaskManager.findById(id).equals("Id not found")) {
+                System.out.println("Введите корректный id");
+            } else {
+                break;
+            }
+        }
+        System.out.println(TaskManager.findById(id));
+    }
+
+    private static void setDescription() {
+        System.out.println("Введите id задачи:");
+        TaskManager.listAllId();
+        int idDes;
+        while(true) {
+            idDes = safeReadInt();
+            if(TaskManager.findById(idDes).equals("Id not found")) {
+                System.out.println("Введите корректный id");
+            } else {
+                break;
+            }
+        }
+        System.out.println("Введите описание для задачи: " + TaskManager.findById(idDes));
+        String description = sc.nextLine();
+        TaskManager.setDescription(idDes, description);
+        System.out.println("Описание для задачи добавлено!");
+    }
+
+    public static void setDeadline() {
+        System.out.println("Введите id задачи:");
+        TaskManager.listAllId();
+        int idDead;
+        while(true) {
+            idDead = safeReadInt();
+            if(TaskManager.findById(idDead).equals("Id not found")) {
+                System.out.println("Введите корректный id");
+            } else {
+                break;
+            }
+        }
+        System.out.println("Введите конечную дату для задачи: " + TaskManager.findById(idDead));
+        String deadline = sc.nextLine();
+        TaskManager.setDeadline(idDead, deadline);
+        System.out.println("Конечная дата для задачи установлена!");
+    }
+
+    private static int safeReadInt() {
+        while(true) {
+            try {
+               return Integer.parseInt(sc.nextLine());
+            } catch (NumberFormatException exc) {
+                System.out.println("Введите корректный Id в целочисленном формате!");
             }
         }
     }
